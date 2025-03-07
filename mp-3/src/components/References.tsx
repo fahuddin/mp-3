bb8ae0e590cbfb7b09f652129ca09bd000610410
unknown <fahuddin@bu.edu>
2025-03-07 10:48:17 -0500

    more content in references

--- mp-3/src/components/References.tsx
+++ mp-3/src/components/References.tsx
@@ -13,6 +13,14 @@ export default function References(){
                             <li>Drake</li>
                             <li>ASAP Rocky</li>
                             <li>Kendrick Lamar</li>
+                            <li>Jackie Chan</li>
+                            <li>Chris Tucker</li>
+                            <li>John Cena</li>
+                            <li>Drake's dad</li>
+                            <li>Kai Cenat</li>
+                            <li>ishowspeed</li>
+                            <li>The rock</li>
+                            <li>Vin diesel</li>
                         </ul>
                     
     
